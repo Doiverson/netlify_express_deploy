a930552f977a7148d27ddd46edbcd8d34ac85eea
Doiverson <doiverson.canada@gmail.com>
2022-04-21 19:24:13 -0700

    update

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+   res.send("Fuck you!!!")
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
